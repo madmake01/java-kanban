6846c7f5ee8fd03a8968c932e8f2f28c2b148808
liubov belik <vekrisa01@gmail.com>
2025-02-17 15:50:00 +0300

    new error messages

--- src/exception/TaskExceptionMessage.java
+++ src/exception/TaskExceptionMessage.java
@@ -4,11 +4,11 @@ public class TaskExceptionMessage {
     public static final String TASK_DOES_NOT_EXIST = "Task does not exist: ";
 
     public static final String EPIC_DOES_NOT_EXIST = "Epic does not exist: ";
+    public static final String NEW_EPIC_SHOULD_BE_EMPTY = "New Epic should be empty: ";
+    public static final String EPICS_SUBTASKS_SHOULD_BE_EQUAL = "Epic subtasks should be equal: ";
 
     public static final String SUBTASK_DOES_NOT_EXIST = "Subtask does not exist: ";
-    public static final String SUBTASK_DOES_NOT_ASSOCIATED = "Subtask does not associated: ";
     public static final String SUBTASK_ALREADY_ASSOCIATED = "Subtask already associated: ";
-    public static final String ASSOCIATED_EPIC_DOES_NOT_EXIST = "Associated with Subtask Epic does not exist: ";
     public static final String SUBTASK_ALREADY_EXISTS = "Subtask already exists: ";
 
     private TaskExceptionMessage() {
