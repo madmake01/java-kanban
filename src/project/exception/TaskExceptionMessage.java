96a9b7de8822fb6eb22a2c41bd36e2ad99694102
liubov belik <vekrisa01@gmail.com>
2025-03-02 11:37:56 +0300

    error fix: cannot create epic with non-default status

--- src/project/exception/TaskExceptionMessage.java
+++ src/project/exception/TaskExceptionMessage.java
@@ -1,16 +1,17 @@
 package project.exception;
 
 public class TaskExceptionMessage {
-    public static final String ID_SHOULD_BE_DEFAULT = "Task does not have a default id: ";
+    public static final String ID_SHOULD_BE_DEFAULT = "Task does not have a default id";
 
-    public static final String TASK_DOES_NOT_EXIST = "Task does not exist: ";
+    public static final String TASK_DOES_NOT_EXIST = "Task does not exist";
 
-    public static final String EPIC_DOES_NOT_EXIST = "Epic does not exist: ";
-    public static final String NEW_EPIC_SHOULD_BE_EMPTY = "New Epic should be empty: ";
-    public static final String EPICS_SUBTASKS_SHOULD_BE_EQUAL = "Epic subtasks should be equal: ";
+    public static final String EPIC_DOES_NOT_EXIST = "Epic does not exist";
+    public static final String NEW_EPIC_SHOULD_BE_EMPTY = "New Epic should be empty";
+    public static final String EPICS_SUBTASKS_SHOULD_BE_EQUAL = "Epic subtasks should be equal";
+    public static final String NEW_EPIC_SHOULD_HAVE_DEFAULT_STATUS = "New Epic should have default status";
 
-    public static final String SUBTASK_DOES_NOT_EXIST = "Subtask does not exist: ";
-    public static final String SUBTASK_SHOULD_HAVE_EQUAL_EPICS = "Subtasks should have equal epics: ";
+    public static final String SUBTASK_DOES_NOT_EXIST = "Subtask does not exist";
+    public static final String SUBTASK_SHOULD_HAVE_EQUAL_EPICS = "Subtasks should have equal epics";
 
     private TaskExceptionMessage() {
     }
--- src/project/util/TaskValidator.java
+++ src/project/util/TaskValidator.java
@@ -1,18 +1,22 @@
 package project.util;
 
+import project.enums.Status;
 import project.exception.EntityAlreadyExistsException;
 import project.model.Epic;
 import project.model.Subtask;
 import project.model.Task;
 
+import java.security.InvalidParameterException;
 import java.util.HashSet;
 import java.util.List;
 
 import static project.exception.TaskExceptionMessage.EPICS_SUBTASKS_SHOULD_BE_EQUAL;
 import static project.exception.TaskExceptionMessage.ID_SHOULD_BE_DEFAULT;
 import static project.exception.TaskExceptionMessage.NEW_EPIC_SHOULD_BE_EMPTY;
+import static project.exception.TaskExceptionMessage.NEW_EPIC_SHOULD_HAVE_DEFAULT_STATUS;
 import static project.exception.TaskExceptionMessage.SUBTASK_SHOULD_HAVE_EQUAL_EPICS;
 import static project.util.AbstractTaskBuilder.DEFAULT_ID;
+import static project.util.AbstractTaskBuilder.DEFAULT_STATUS;
 
 public class TaskValidator {
 
@@ -30,6 +34,9 @@ public class TaskValidator {
 
         if (!epic.getSubTaskIds().isEmpty()) {
             throw new EntityAlreadyExistsException(NEW_EPIC_SHOULD_BE_EMPTY);
+        }
+        if(epic.getStatus() != DEFAULT_STATUS) {
+            throw new InvalidParameterException(NEW_EPIC_SHOULD_HAVE_DEFAULT_STATUS);
         }
     }
 
