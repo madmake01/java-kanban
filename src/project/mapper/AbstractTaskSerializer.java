a4b3524621f6b7f617e2fa25f487ffe8fb4f6fe6
liubov belik <vekrisa01@gmail.com>
2025-04-19 21:40:50 +0300

    feat: add time data support to serialization and deserialization

--- src/project/mapper/AbstractTaskSerializer.java
+++ src/project/mapper/AbstractTaskSerializer.java
@@ -7,6 +7,8 @@ import project.model.Subtask;
 import project.model.Task;
 import project.util.AbstractTaskBuilder;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,99 +18,101 @@ import java.util.function.Supplier;
 
 public class AbstractTaskSerializer {
 
-    private static final int ID_INDEX = 0;
-    private static final int TASK_TYPE = 1;
-    private static final int NAME_INDEX = 2;
-    private static final int DESCRIPTION_INDEX = 3;
-    private static final int STATUS_INDEX = 4;
-    private static final int ADDITIONAL_INFO = 5;
-
     private static final String FIELD_DELIMITER = ",";
-
-    private static final Map<String, Function<String[], AbstractTask>> mapper;
-
+    private static final Map<String, Function<String[], AbstractTask>> mapper = new HashMap<>();
 
     static {
-        mapper = new HashMap<>();
+        mapper.put(Epic.class.getSimpleName(), array -> {
+            List<Integer> subtaskIds = extractSubtaskIds(array);
 
-        mapper.put(Epic.class.getSimpleName(), (array -> {
-            List<Integer> subtaskId = extractSubtaskId(array);
-            Epic.Builder builder = getTaskBuilder(array, Epic.Builder::new);
-            return builder.setSubtaskIds(subtaskId)
-                    .build();
-        }));
+            LocalDateTime end = null;
+            String endString = array[7];
+            if (!endString.isEmpty()) {
+                end = LocalDateTime.parse(endString);
+            }
 
-        mapper.put(Task.class.getSimpleName(), (array -> {
-            Task.Builder builder = getTaskBuilder(array, Task.Builder::new);
+            Epic.Builder builder = getTaskBuilder(array, Epic.Builder::new)
+                    .setSubtaskIds(subtaskIds)
+                    .setEndTime(end);
             return builder.build();
-        }));
+        });
 
-        mapper.put(Subtask.class.getSimpleName(), (array -> {
-            Subtask.Builder builder = getTaskBuilder(array, Subtask.Builder::new);
-            int epicId = Integer.parseInt(array[ADDITIONAL_INFO]);
-            return builder.setEpicId(epicId)
-                    .build();
+        mapper.put(Task.class.getSimpleName(), array ->
+                getTaskBuilder(array, Task.Builder::new).build()
+        );
 
-        }));
+        mapper.put(Subtask.class.getSimpleName(), array ->
+                getTaskBuilder(array, Subtask.Builder::new)
+                        .setEpicId(Integer.parseInt(array[array.length - 1]))
+                        .build()
+        );
     }
 
     private AbstractTaskSerializer() {
     }
 
     public static AbstractTask deserialize(String serialized) {
-        String[] split = serialized.split(FIELD_DELIMITER);
-        Function<String[], AbstractTask> taskFactory = mapper.get(split[TASK_TYPE]);
-
-        if (taskFactory == null) {
-            throw new IllegalArgumentException("Unknown task type: " + split[TASK_TYPE]);
+        String[] split = serialized.split(FIELD_DELIMITER, -1);
+        Function<String[], AbstractTask> factory = mapper.get(split[1]);
+        if (factory == null) {
+            throw new IllegalArgumentException("Unknown task type: " + split[1]);
         }
-        return taskFactory.apply(split);
+        return factory.apply(split);
     }
 
     public static String serialize(AbstractTask task) {
         List<String> fields = extractBasicFields(task);
 
-        if (task instanceof Epic epic) {
-            fields.addAll(epic.getSubtaskIds().stream()
+        switch (task) {
+            case Epic epic -> fields.addAll(epic.getSubtaskIds().stream()
                     .map(String::valueOf)
                     .toList());
-        } else if (task instanceof Subtask subtask) {
-            fields.add(String.valueOf(subtask.getEpicId()));
+            case Subtask subtask -> fields.add(String.valueOf(subtask.getEpicId()));
+            case Task ignored -> fields.add("");
+            default -> throw new IllegalArgumentException("Unknown task type: " + task);
         }
 
         return String.join(FIELD_DELIMITER, fields);
     }
 
 
-    public static <T extends AbstractTask, B extends AbstractTaskBuilder<T, B>> B getTaskBuilder(
+    private static <T extends AbstractTask, B extends AbstractTaskBuilder<T, B>> B getTaskBuilder(
             String[] array,
             Supplier<B> builderSupplier
     ) {
+        String startTimeString = array[5];
+
+        LocalDateTime startTime = startTimeString.isEmpty() ? null : LocalDateTime.parse(startTimeString);
+        String durationString = array[6];
+        Duration duration = durationString.isEmpty() ? null : Duration.parse(durationString);
         return builderSupplier.get()
-                .setId(Integer.parseInt(array[ID_INDEX]))
-                .setName(array[NAME_INDEX])
-                .setDescription(array[DESCRIPTION_INDEX])
-                .setStatus(Status.valueOf(array[STATUS_INDEX].toUpperCase()));
+                .setId(Integer.parseInt(array[0]))
+                .setName(array[2])
+                .setDescription(array[3])
+                .setStatus(Status.valueOf(array[4].toUpperCase()))
+                .setStartTime(startTime)
+                .setDuration(duration);
     }
 
-
-    private static List<Integer> extractSubtaskId(String[] array) {
-        List<Integer> subtasksId = new ArrayList<>();
-        for (int i = ADDITIONAL_INFO; i < array.length; i++) {
-            subtasksId.add(Integer.parseInt(array[i]));
+    private static List<Integer> extractSubtaskIds(String[] array) {
+        List<Integer> ids = new ArrayList<>();
+        for (int i = 8; i < array.length; i++) {
+            ids.add(Integer.parseInt(array[i]));
         }
-        return subtasksId;
+        return ids;
     }
-
 
     private static List<String> extractBasicFields(AbstractTask task) {
         List<String> fields = new ArrayList<>();
 
-        fields.add(ID_INDEX, String.valueOf(task.getId()));
-        fields.add(TASK_TYPE, task.getClass().getSimpleName());
-        fields.add(NAME_INDEX, task.getName());
-        fields.add(DESCRIPTION_INDEX, task.getDescription());
-        fields.add(STATUS_INDEX, task.getStatus().toString());
+        fields.add(String.valueOf(task.getId()));
+        fields.add(task.getClass().getSimpleName());
+        fields.add(task.getName());
+        fields.add(task.getDescription());
+        fields.add(task.getStatus().toString());
+        fields.add(task.getStartTime().map(LocalDateTime::toString).orElse(""));
+        fields.add(task.getDuration().map(Duration::toString).orElse(""));
+        fields.add(task.getEndTime().map(LocalDateTime::toString).orElse(""));
 
         return fields;
     }
